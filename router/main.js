06292562dba7efd46274fce778e18409c4355ded
Joseph DeVictoria <jldevictoria@gmail.com>
2016-10-07 01:41:23 +0000

    Added Ace.js for text entry.  Updated CSS a lot!

--- router/main.js
+++ router/main.js
@@ -2,6 +2,7 @@
 // Author:      Joseph DeVictoria
 // Date:        Oct_3_2016
 // Purpose:     Main router for Mutable on express.
+
 // Web App Path Requests:
 module.exports = function(app) {
     app.get('/', function(req, res) {
@@ -10,10 +11,24 @@ module.exports = function(app) {
     });
     
     app.get('/jobs', function(req, res) {
-        res.render('jobs');
+        res.render('jobs', {id: "1337"});
         console.log('User at ' + req.headers['x-forwarded-for'] + ' requested the Jobs page!');
     });
     
+    app.get('/job/:id', function(req, res) {
+        res.render('job', {id: req.params.id});
+        console.log('User at ' + req.headers['x-forwarded-for'] + ' requested the ' + req.params.id + ' Job page!');
+    });
+    
+    app.get('/apply', function(req, res) {
+        res.render('apply');
+        console.log('User at ' + req.headers['x-forwarded-for'] + ' requested the Apply page!');
+    });
+    
+    app.get('/challenge', function(req, res) {
+        res.render('challenge');
+        console.log('User at ' + req.headers['x-forwarded-for'] + ' requested the Challenge page!');
+    });
     app.get('/applications', function(req, res) {
         res.render('applications');
         console.log('User at ' + req.headers['x-forwarded-for'] + ' requested the Applications page!');
